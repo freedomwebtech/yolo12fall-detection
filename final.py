61ebedb5f5933aea2056bd985c9a2ded6859df36
freedomwebtech <48402744+freedomwebtech@users.noreply.github.com>
2025-06-30 09:04:23 +0530

    Add files via upload

---
+++ final.py
@@ -0,0 +1,81 @@
+import cv2
+from ultralytics import YOLO
+import cvzone
+
+# Load model and get class names
+model = YOLO('yolo12s.pt')
+names = model.names
+
+# Video source
+cap = cv2.VideoCapture("falldown.mp4")
+
+
+frame_count=0
+
+# Mouse callback (for debugging pixel position)
+def RGB(event, x, y, flags, param):
+    if event == cv2.EVENT_MOUSEMOVE:
+        print(f"Mouse moved to: [{x}, {y}]")
+
+cv2.namedWindow("RGB")
+cv2.setMouseCallback("RGB", RGB)
+
+# Main loop
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_count += 1
+    if frame_count % 3 != 0:
+        continue
+
+    frame = cv2.resize(frame, (1020,600))
+    results = model.track(frame, persist=True,classes=[0])
+
+    if results[0].boxes.id is not None:
+        ids = results[0].boxes.id.cpu().numpy().astype(int)
+        boxes = results[0].boxes.xyxy.cpu().numpy().astype(int)
+        class_ids = results[0].boxes.cls.int().cpu().tolist()
+
+        for track_id, box, class_id in zip(ids, boxes, class_ids):
+            x1, y1, x2, y2 = box
+            cx = int((x1 + x2) / 2)
+            cy = int((y1 + y2) / 2)
+            name = names[class_id]
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cvzone.putTextRect(frame, f'{name}', (x1, y1), 1, 1)
+            h=y2-y1
+            w=x2-x1
+            thresh=h-w
+#            print(thresh) 
+            if 'person' in name:
+                if thresh <0:
+                   cvzone.putTextRect(frame,f'{"person_fall"}',(x1,y2 +12),1,1)
+                   cv2.rectangle(frame,(x1,y1),(x2,y2),(0,0,255),2)
+
+          
+
+          
+
+ 
+
+            
+
+    # Draw IN/OUT count and lines
+#    cvzone.putTextRect(frame, f'IN: {in_count}', (316, 33), scale=2, thickness=2,
+#                       colorT=(255, 255, 255), colorR=(0, 128, 0))
+#    cvzone.putTextRect(frame, f'OUT: {out_count}', (316, 100), scale=2, thickness=2,
+#                       colorT=(255, 255, 255), colorR=(0, 0, 255))
+
+
+    # Show frame
+    cv2.imshow("RGB", frame)
+
+    # Exit on ESC
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
